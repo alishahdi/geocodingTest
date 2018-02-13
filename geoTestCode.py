2421edf1252ce6db26d7d2dbe4af27f15da345eb
Ali Shahdi <alishahdi@verbsurgical.com>
2018-02-12 19:30:40 -0800

    First commit

---
+++ geoTestCode.py
@@ -0,0 +1,105 @@
+# Ali Shahdi
+# Coding test
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urllib2
+import json
+
+# Class for handling REST APIs
+class RESTHandler(BaseHTTPRequestHandler): 
+	# Generating the header for HTTP response
+	def _set_headers(self): 
+		self.send_response(200)
+        	self.send_header('Content-type', 'text/html')
+	        self.end_headers()
+
+	# Respond to GET requests
+        def do_GET(self): 		
+		# Ignore faivcon requests
+		if self.path == '/favicon.ico': 
+			pass
+		else:
+			# Generate the header
+		        self._set_headers() 
+			# Send geocoding requests using Google and HERE APIs and parse the lat/lng results for the address	
+			jsonResponse = self.geoCode() 
+			# Send back the result to the requester
+		        self.wfile.write(jsonResponse)
+	
+	# Not doing anything on REST POST requests
+	def POST(self):
+		pass
+
+    	# Geocoding the address
+        def geoCode(self): 
+		# Get the address from the requester URL path	
+		address = self.path 
+
+		# Try using Google APIs first (failure to connect be simulated by changing this URL)
+		try: 
+			# Google geocoding API GET request
+			geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyDzVSbsHjzNyN6LEcASEJ29Cmrj9ET4qZo" % address 
+			request = urllib2.urlopen(geoURL)
+			jsonResponse = json.loads(request.read())
+		
+			# If the returned staus is OK parse lat/lng and return the values
+			if out['status'] == 'OK': 
+   				plat = jsonResponse['results'][0]['geometry']['location']['lat']
+   				plng = jsonResponse['results'][0]['geometry']['location']['lng']
+
+				# Creating the JSON output from the lat/lng info
+				data = {}
+				data['status'] = 'OK'
+				data['lat'] = plat
+				data['lng'] = plng
+				data['source'] = 'Google'
+				jsonOutput = json.dumps(data)
+				return jsonOutput
+			# Google servers responded but the address was invalid
+			else:
+				data = {}
+				data['status'] = 'Invalid'
+				data['source'] = 'Google'
+				jsonOutput = json.dumps(data)
+				return jsonOutput
+	
+		# Google servers cannot be reaced, trying to use HERE APIs as the backup (catching all the exceptions)	
+		except:
+			try:
+				print("Google failed trying HERE")
+				
+				#HERE geocoding API GET request
+				geoURL = "https://geocoder.cit.api.here.com/6.2/geocode.json?app_id=ykrI6wAjdahtpMwgZxhh&app_code=t1R7EyxS-G_q-VT78OcikA&searchtext=%s" % address 
+
+				req = urllib2.urlopen(geoURL)
+				out = json.loads(req.read())
+			
+				# Check to see if the result is valid and return the values
+				if len(out['Response']['View']) > 0: 
+	
+		   			plat = out['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Latitude']
+		   			plng = out['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Longitude']
+					return 'for ' + self.path + ' HERE lat is = ' + str(plat) + ' and lng is = ' + str(plng)
+				else:
+					# HERE servers has responded but the address was invalid
+					return "No Results: Please enter a valid address" 
+			# HERE APIs are also failing
+			except: 
+				return "All servers down. Please try again!"
+			
+# Running the HTTP server on PORT 8888        
+def runServer(serverClass = HTTPServer, handlerClass = RESTHandler, port = 8888): 
+   	address = ('', port)
+	httpServer = serverClass(address, handlerClass)
+	print 'Starting the http server on port %d ...' % port
+	try:
+	        httpServer.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	print 'Stopping the HTTP server ...'
+	http_server.server_close()
+
+
+# __main__
+if __name__ == "__main__":
+        runServer()
